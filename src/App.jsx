4363b9861c8eac464af1bd214756e741b9d7469f
Shehab Eldin Said <92757027+Shehab1001@users.noreply.github.com>
2024-05-14 22:43:21 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shehab Eldin Saeed",
+  title: "Web Developer",
+  email: "Shehab.eldin.said1@gmail.com",
+  gitHub: "shehab1001",
+  instagram: "shehab_afify",
+  linkedIn: "shehab-eldin-said-88b185222",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
